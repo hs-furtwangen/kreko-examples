de76c284a3996be9186c38419ab255f3350ce6d1
Norbert Schnell <norbert.schnell@hs-furtwangen.de>
2021-03-27 23:41:54 +0100

    added photo booth example and get user media manager

---
+++ photo-booth/scripts.js
@@ -0,0 +1,75 @@
+"use strict";
+var photoBooth;
+(function (photoBooth) {
+    const video = document.getElementById("video");
+    const image = document.getElementById("image");
+    const instructions = document.getElementById("instructions");
+    const canvas = document.createElement("canvas");
+    let imageWidth = null;
+    let imageHeight = null;
+    let videoX = null;
+    let videoY = null;
+    // create start screen and register 
+    const userMediaManager = new UserMediaManager({ video: true, audio: false });
+    userMediaManager.videoElement = video;
+    // create start screen and register web audio manager
+    const startScreen = new StartScreen("start-screen");
+    startScreen.addResourceManager(userMediaManager);
+    // start (creates audio context )
+    startScreen.start().then(() => {
+        video.play().then(() => {
+            adaptElements();
+            window.addEventListener("resize", adaptElements);
+            video.addEventListener("click", takeSnapshot);
+            image.addEventListener("click", resetVideo);
+        });
+    });
+    function takeSnapshot() {
+        const context = canvas.getContext("2d");
+        context.drawImage(video, videoX, videoY, imageWidth, imageHeight, 0, 0, imageWidth, imageHeight);
+        const srcDataUrl = canvas.toDataURL("image/png");
+        image.setAttribute("src", srcDataUrl);
+        video.classList.add("hide");
+        image.classList.remove("hide");
+        instructions.innerHTML = "tap screen to display camera capture";
+    }
+    function resetVideo() {
+        image.classList.add("hide");
+        video.classList.remove("hide");
+        instructions.innerHTML = "tap screen to take a snapshot";
+    }
+    // adapt video element to current screen size
+    function adaptElements() {
+        const rect = document.body.getBoundingClientRect();
+        const screenWidth = rect.width;
+        const screenHeight = rect.height;
+        const videoWidth = video.videoWidth;
+        const videoHeight = video.videoHeight;
+        const widthRatio = videoWidth / screenWidth;
+        const heightRatio = videoHeight / screenHeight;
+        const ratio = Math.min(widthRatio, heightRatio);
+        // set image size and offset
+        imageWidth = Math.floor(ratio * screenWidth + 0.5);
+        imageHeight = Math.floor(ratio * screenHeight + 0.5);
+        videoX = Math.max(0, Math.floor(0.5 * (videoWidth - imageWidth) + 0.5));
+        videoY = Math.max(0, Math.floor(0.5 * (videoHeight - imageHeight) + 0.5));
+        // adapt canvas size to input video size
+        canvas.width = imageWidth;
+        canvas.height = imageHeight;
+        // adapt canvas size to screen
+        image.width = screenWidth;
+        image.height = screenHeight;
+        // calculate video screen size and offset
+        const videoScreenWidth = Math.floor(videoWidth / ratio + 0.5);
+        const videoScreenHeight = Math.floor(videoHeight / ratio + 0.5);
+        const videoScreenX = Math.floor(videoX / ratio + 0.5);
+        const videoScreenY = Math.floor(videoY / ratio + 0.5);
+        // resize and position video an screen
+        video.width = videoScreenWidth;
+        video.height = videoScreenHeight;
+        video.style.left = `${-videoScreenX}px`;
+        video.style.top = `${-videoScreenY}px`;
+        resetVideo();
+    }
+})(photoBooth || (photoBooth = {}));
+//# sourceMappingURL=scripts.js.map
---
+++ photo-booth/scripts.ts
@@ -0,0 +1,89 @@
+namespace photoBooth {
+  const video: HTMLVideoElement = <HTMLVideoElement>document.getElementById("video");
+  const image: HTMLImageElement = <HTMLImageElement>document.getElementById("image");
+  const instructions: HTMLDivElement = <HTMLDivElement>document.getElementById("instructions");
+  const canvas: HTMLCanvasElement = document.createElement("canvas");
+  let imageWidth: number = null;
+  let imageHeight: number = null;
+  let videoX: number = null;
+  let videoY: number = null;
+
+  // create start screen and register 
+  const userMediaManager: UserMediaManager = new UserMediaManager({ video: true, audio: false });
+  userMediaManager.videoElement = video;
+
+  // create start screen and register web audio manager
+  const startScreen: StartScreen = new StartScreen("start-screen");
+  startScreen.addResourceManager(userMediaManager);
+
+  // start (creates audio context )
+  startScreen.start().then(() => {
+    video.play().then(() => {
+      adaptElements();
+
+      window.addEventListener("resize", adaptElements);
+      video.addEventListener("click", takeSnapshot);
+      image.addEventListener("click", resetVideo);
+    });
+  });
+
+  function takeSnapshot(): void {
+    const context: CanvasRenderingContext2D = canvas.getContext("2d");
+    context.drawImage(video, videoX, videoY, imageWidth, imageHeight, 0, 0, imageWidth, imageHeight);
+
+    const srcDataUrl: string = canvas.toDataURL("image/png");
+    image.setAttribute("src", srcDataUrl);
+
+    video.classList.add("hide");
+    image.classList.remove("hide");
+
+    instructions.innerHTML = "tap screen to display camera capture";
+  }
+
+  function resetVideo(): void {
+    image.classList.add("hide");
+    video.classList.remove("hide");
+
+    instructions.innerHTML = "tap screen to take a snapshot";
+  }
+
+  // adapt video element to current screen size
+  function adaptElements(): void {
+    const rect: DOMRect = document.body.getBoundingClientRect();
+    const screenWidth: number = rect.width;
+    const screenHeight: number = rect.height;
+    const videoWidth: number = video.videoWidth;
+    const videoHeight: number = video.videoHeight;
+    const widthRatio: number = videoWidth / screenWidth;
+    const heightRatio: number = videoHeight / screenHeight;
+    const ratio: number = Math.min(widthRatio, heightRatio);
+
+    // set image size and offset
+    imageWidth = Math.floor(ratio * screenWidth + 0.5);
+    imageHeight = Math.floor(ratio * screenHeight + 0.5);
+    videoX = Math.max(0, Math.floor(0.5 * (videoWidth - imageWidth) + 0.5));
+    videoY = Math.max(0, Math.floor(0.5 * (videoHeight - imageHeight) + 0.5));
+
+    // adapt canvas size to input video size
+    canvas.width = imageWidth;
+    canvas.height = imageHeight;
+
+    // adapt canvas size to screen
+    image.width = screenWidth;
+    image.height = screenHeight;
+
+    // calculate video screen size and offset
+    const videoScreenWidth: number = Math.floor(videoWidth / ratio + 0.5);
+    const videoScreenHeight: number = Math.floor(videoHeight / ratio + 0.5);
+    const videoScreenX: number = Math.floor(videoX / ratio + 0.5);
+    const videoScreenY: number = Math.floor(videoY / ratio + 0.5);
+
+    // resize and position video an screen
+    video.width = videoScreenWidth;
+    video.height = videoScreenHeight;
+    video.style.left = `${-videoScreenX}px`;
+    video.style.top = `${-videoScreenY}px`;
+
+    resetVideo();
+  }
+}
